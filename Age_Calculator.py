f20b7e73064752008b389eff33293f5f1db0528a
chembeti sai deeraj <sr283909@gmail.com>
2026-06-15 18:36:22 +0530

    Update Age_Calculator.py

--- Age_Calculator.py
+++ Age_Calculator.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from datetime import datetime
 from tkinter import messagebox
+
 window = tk.Tk()
 window.title("Age Calculator")
 window.geometry("350x300")
@@ -10,12 +11,20 @@ tk.Label(window, text="Birth Year").pack(pady=5)
 
 year_entry = tk.Entry(window)
 year_entry.pack()
+
 def calculate_age():
-    # Get the input from the Entry box
-    birth_year = year_entry.get()
+    birth_year = year_entry.get().strip()
+
     if birth_year.isdigit():
+        birth_year = int(birth_year)
         current_year = datetime.now().year
-        age = current_year - int(birth_year)
+
+        if birth_year > current_year or birth_year < 1900:
+            messagebox.showwarning("Warning", "Enter a valid year!")
+            return
+
+        age = current_year - birth_year
+
         if age < 13:
             category = "Child"
         elif age < 20:
@@ -25,26 +34,22 @@ def calculate_age():
         else:
             category = "Senior"
 
-        result_label.config(
-        text=f"Age: {age}\nCategory: {category}"
-)
+        result_label.config(text=f"Age: {age}\nCategory: {category}")
+
     else:
-        messagebox.showwarning(
-    "Warning",
-    "Please enter a valid year"
-)
+        messagebox.showwarning("Warning", "Please enter numbers only")
+
 calculate_button = tk.Button(
     window,
     text="Calculate Age",
     command=calculate_age,
     bg="#4CAF50",
     fg="white",
-    activebackground="#2E7D32",
-    activeforeground="white",
     font=("Arial", 10, "bold")
 )
 
-calculate_button.pack()
+calculate_button.pack(pady=10)
+
 result_label = tk.Label(window, text="Your Age:")
 result_label.pack(pady=20)
 
